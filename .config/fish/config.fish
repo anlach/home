3360e7543daabf2f9bba3393df6f0c7e1e6510c6
Andrew LaCharite <adlacharite@gmail.com>
2020-05-05 13:23:17 +0000

    add conda init

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,8 @@
 alias l="ls -altr"
 
+if [ -e /home/anlach/miniconda3/bin/conda ]
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/anlach/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+end
